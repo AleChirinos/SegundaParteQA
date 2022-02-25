3daffeb9af1609e5c49b1804d4d78da1f9289a7b
AleChirinos <alejandra.chirinos2702@gmail.com>
2022-02-25 19:51:31 -0400

    Tarea 10 Comparando JSONs - Alejandra Chirinos

--- src/test/java/tarea10Test/Tarea10JSONTest.java
+++ src/test/java/tarea10Test/Tarea10JSONTest.java
@@ -24,8 +24,8 @@ public class Tarea10JSONTest {
                 "\"createTime\": 1545645456\r\n" +
                 "}";
 
-        Tarea10JSON t = new Tarea10JSON();
-        Assertions.assertThrows(Exception.class, ()->{t.jsonAreEqual(json1, json2, "Error");});
+        Tarea10JSON tarea10JSON = new Tarea10JSON();
+        Assertions.assertThrows(Exception.class, ()->{tarea10JSON.jsonAreEqual(json1, json2, "Error");});
     }
 
     @Test
@@ -48,8 +48,8 @@ public class Tarea10JSONTest {
                 "\"update\": true\r\n" +
                 "}";
 
-        Tarea10JSON t = new Tarea10JSON();
-        t.jsonAreEqual(json1, json2, "Error");
+        Tarea10JSON tarea10JSON = new Tarea10JSON();
+        tarea10JSON.jsonAreEqual(json1, json2, "Error");
     }
 
     @Test
@@ -61,7 +61,55 @@ public class Tarea10JSONTest {
         String json2 = "{\r\n" +
                 "}";
 
-        Tarea10JSON t = new Tarea10JSON();
-        Assertions.assertThrows(Exception.class, ()->{t.jsonAreEqual(json1, json2, "Error");});
+        Tarea10JSON tarea10JSON = new Tarea10JSON();
+        Assertions.assertThrows(Exception.class, ()->{tarea10JSON.jsonAreEqual(json1, json2, "Error");});
+    }
+
+    @Test
+    public void jsonAreEqualTest4() throws Exception {
+        String json1 = "{\r\n" +
+                "\"name\":\"eynar\",\r\n" +
+                "\"icon\": 5,\r\n" +
+                "\"deleted\": true,\r\n" +
+                "\"id\": 5465456,\r\n" +
+                "\"createTime\": \"IGNORE\",\r\n" +
+                "\"update\": true\r\n" +
+                "}";
+
+        String json2 = "{\r\n" +
+                "\"name\":\"eynar\",\r\n" +
+                "\"icon\": 5,\r\n" +
+                "\"deleted\": true,\r\n" +
+                "\"id\": 5465456,\r\n" +
+                "\"createTime\": \"abcd\",\r\n" +
+                "\"update\": true\r\n" +
+                "}";
+
+        Tarea10JSON tarea10JSON = new Tarea10JSON();
+        tarea10JSON.jsonAreEqual(json1, json2, "ErrorEnMetodo");
+    }
+
+    @Test
+    public void jsonAreEqualTest5() throws Exception {
+        String json1 = "{\r\n" +
+                "\"name\":\"eynar\",\r\n" +
+                "\"icon\": 5,\r\n" +
+                "\"deleted\": true,\r\n" +
+                "\"id\": 5465456,\r\n" +
+                "\"createTime\": false,\r\n" +
+                "\"update\": true\r\n" +
+                "}";
+
+        String json2 = "{\r\n" +
+                "\"name\":\"eynar\",\r\n" +
+                "\"icon\": 5,\r\n" +
+                "\"deleted\": true,\r\n" +
+                "\"id\": 5465456,\r\n" +
+                "\"createTime\": \"IGNORE\",\r\n" +
+                "\"update\": true\r\n" +
+                "}";
+
+        Tarea10JSON tarea10JSON = new Tarea10JSON();
+        tarea10JSON.jsonAreEqual(json1, json2, "ErrorEnMetodo");
     }
 }
