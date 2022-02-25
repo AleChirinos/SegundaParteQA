358c514fa09b56d718f99747114c5a0f317520a3
AleChirinos <alejandra.chirinos2702@gmail.com>
2022-02-25 19:29:32 -0400

    Tarea 10 Comparando JSONs - Alejandra Chirinos

---
+++ src/test/java/tarea10Test/Tarea10JSONTest.java
@@ -0,0 +1,67 @@
+package tarea10Test;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import tarea10.Tarea10JSON;
+
+public class Tarea10JSONTest {
+    @Test
+    public void jsonAreEqualTest1() throws Exception {
+        String json1 = "{\r\n" +
+                "\"name\":\"eynar\",\r\n" +
+                "\"icon\": 5,\r\n" +
+                "\"deleted\": true,\r\n" +
+                "\"id\": 5465456,\r\n" +
+                "\"createTime\": \"IGNORE\",\r\n" +
+                "\"update\": true\r\n" +
+                "}";
+
+        String json2 = "{\r\n" +
+                "\"name\":\"eynar5\",\r\n" +
+                "\"icon\": 5,\r\n" +
+                "\"deleted\": true,\r\n" +
+                "\"id\": 5465456,\r\n" +
+                "\"createTime\": 1545645456\r\n" +
+                "}";
+
+        Tarea10JSON t = new Tarea10JSON();
+        Assertions.assertThrows(Exception.class, ()->{t.jsonAreEqual(json1, json2, "Error");});
+    }
+
+    @Test
+    public void jsonAreEqualTest2() throws Exception {
+        String json1 = "{\r\n" +
+                "\"name\":\"eynar\",\r\n" +
+                "\"icon\": 5,\r\n" +
+                "\"deleted\": true,\r\n" +
+                "\"id\": 5465456,\r\n" +
+                "\"createTime\": \"IGNORE\",\r\n" +
+                "\"update\": true\r\n" +
+                "}";
+
+        String json2 = "{\r\n" +
+                "\"name\":\"eynar\",\r\n" +
+                "\"icon\": 5,\r\n" +
+                "\"deleted\": true,\r\n" +
+                "\"id\": 5465456,\r\n" +
+                "\"createTime\": \"IGNORE\",\r\n" +
+                "\"update\": true\r\n" +
+                "}";
+
+        Tarea10JSON t = new Tarea10JSON();
+        t.jsonAreEqual(json1, json2, "Error");
+    }
+
+    @Test
+    public void jsonAreEqualTest3() throws Exception {
+        String json1 = "{\r\n" +
+                "\"name\":\"eynar\",\r\n" +
+                "}";
+
+        String json2 = "{\r\n" +
+                "}";
+
+        Tarea10JSON t = new Tarea10JSON();
+        Assertions.assertThrows(Exception.class, ()->{t.jsonAreEqual(json1, json2, "Error");});
+    }
+}
